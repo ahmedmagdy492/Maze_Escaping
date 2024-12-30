e3f2220a74d59b18ed91aa9c5408851525e15e30
ahmed magdy <ahmedmagdy492@yahoo.com>
2024-12-30 16:03:30 +0200

    made 3d walls more thick in the z axis

--- src/MazeRenderer.cpp
+++ src/MazeRenderer.cpp
@@ -17,16 +17,58 @@
 #include <stack>
 
 // data section
+//static float wallVertexData[] = {
+//	-0.5f, 0.5f, 0.0f,   0.0f, 0.0f,
+//	 0.5f, 0.5f, 0.0f,   1.0f, 0.0f,
+//	-0.5f, -0.5f, 0.0f,  0.0f, 1.0f,
+//	 0.5f, -0.5f, 0.0f,  1.0f, 1.0f
+//};
+
 static float wallVertexData[] = {
-	-0.5f, 0.5f, 0.0f,   0.0f, 0.0f,
-	 0.5f, 0.5f, 0.0f,   1.0f, 0.0f,
-	-0.5f, -0.5f, 0.0f,  0.0f, 1.0f,
-	 0.5f, -0.5f, 0.0f,  1.0f, 1.0f
+	// Front face
+   -0.5f,  0.5f,  0.5f,   0.0f, 0.0f,  // Top-left
+	0.5f,  0.5f,  0.5f,   1.0f, 0.0f,  // Top-right
+   -0.5f, -0.5f,  0.5f,   0.0f, 1.0f,  // Bottom-left
+	0.5f, -0.5f,  0.5f,   1.0f, 1.0f,  // Bottom-right
+
+	// Back face
+	-0.5f,  0.5f, -0.5f,   0.0f, 0.0f,  // Top-left
+	 0.5f,  0.5f, -0.5f,   1.0f, 0.0f,  // Top-right
+	-0.5f, -0.5f, -0.5f,   0.0f, 1.0f,  // Bottom-left
+	 0.5f, -0.5f, -0.5f,   1.0f, 1.0f,  // Bottom-right
+
+	 // Left face
+	 -0.5f,  0.5f, -0.5f,   0.0f, 0.0f,  // Top-left
+	 -0.5f,  0.5f,  0.5f,   1.0f, 0.0f,  // Top-right
+	 -0.5f, -0.5f, -0.5f,   0.0f, 1.0f,  // Bottom-left
+	 -0.5f, -0.5f,  0.5f,   1.0f, 1.0f,  // Bottom-right
+
+	 // Right face
+	  0.5f,  0.5f, -0.5f,   0.0f, 0.0f,  // Top-left
+	  0.5f,  0.5f,  0.5f,   1.0f, 0.0f,  // Top-right
+	  0.5f, -0.5f, -0.5f,   0.0f, 1.0f,  // Bottom-left
+	  0.5f, -0.5f,  0.5f,   1.0f, 1.0f,  // Bottom-right
+
+	  // Top face
+	  -0.5f,  0.5f, -0.5f,   0.0f, 0.0f,  // Top-left
+	   0.5f,  0.5f, -0.5f,   1.0f, 0.0f,  // Top-right
+	  -0.5f,  0.5f,  0.5f,   0.0f, 1.0f,  // Bottom-left
+	   0.5f,  0.5f,  0.5f,   1.0f, 1.0f,  // Bottom-right
+
+	   // Bottom face
+	   -0.5f, -0.5f, -0.5f,   0.0f, 0.0f,  // Top-left
+		0.5f, -0.5f, -0.5f,   1.0f, 0.0f,  // Top-right
+	   -0.5f, -0.5f,  0.5f,   0.0f, 1.0f,  // Bottom-left
+		0.5f, -0.5f,  0.5f,   1.0f, 1.0f   // Bottom-right
 };
 
 static unsigned int elementBufferData[] = {
-		0, 2, 3,
-		0, 1, 3
+		0, 1, 2,  1, 3, 2,  // Front face
+		4, 5, 6,  5, 7, 6,  // Back face
+		8, 9, 10, 9, 11, 10,  // Left face
+		12, 13, 14, 13, 15, 14,  // Right face
+		16, 17, 18, 17, 19, 18,  // Top face
+		20, 21, 22, 21, 23, 22
 };
 
 static void RemoveWalls(MazeCell* current, MazeCell* next) {
@@ -136,13 +178,13 @@ void MazeRenderer::Draw3dLine(float x, float y, int w, int h, float angle) {
 	glm::vec3 rotationVec = glm::vec3(0.0f, 1.0f, 0.0f);
 	modelMat = glm::rotate(modelMat, glm::radians(angle), rotationVec);
 
-	glm::vec3 scaleVec = glm::vec3(w, h, 0.0f);
+	glm::vec3 scaleVec = glm::vec3(w, h, 1.0);
 	modelMat = glm::scale(modelMat, scaleVec);
 
 	std::string name = "un_model";
 	curShader->SetUniformMat4(name, glm::value_ptr(modelMat));
 
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, nullptr);
 }
 
 void MazeRenderer::DrawPlane(float x, float y, int w, int h) {
